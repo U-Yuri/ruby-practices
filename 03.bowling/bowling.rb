2ef18caea3ed1a94be166682079e081d95b95824
yuriririn <80314978+yuriririn@users.noreply.github.com>
2023-05-12 19:20:13 +0900

    first commit

---
+++ 03.bowling/bowling.rb
@@ -0,0 +1,14 @@
+# ボウリングスコアプログラム
+
+score = ARGV[0]
+scores = score.split(',')
+shots = []
+scores.each do |s|
+  if s == 'X' # strike
+    shots << 10
+    shots << 0
+  else
+    shots << s.to_i
+  end
+end
+p shots
