5292c657188df2285e2e68537637d93269545df0
yuriririn <80314978+yuriririn@users.noreply.github.com>
2023-05-12 19:45:58 +0900

    変数名を「youbi」から「day_of_week」へ変更

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -11,10 +11,10 @@ puts "\x1B[36;1m#{month}\x1B[37;m月\x1B[36;1m#{year}\x1B[37;m".rjust(38)
 puts "日 月 火 水 木 金 土"
 end_of_date = Date.new(year, month, -1).day
 start_of_date = Date.new(year, month, 1).wday
-youbi = 0
-while youbi < start_of_date
-  youbi += 1
-  if youbi == 1
+day_of_week = 0
+while day_of_week < start_of_date
+  day_of_week += 1
+  if day_of_week == 1
     print " ".rjust(2)
   else
     print " ".rjust(3)
@@ -25,20 +25,20 @@ num = 0
 while num < end_of_date do
   num += 1
   case 
-  when num == today && num < 10 && (youbi)%7 == 0
+  when num == today && num < 10 && (day_of_week)%7 == 0
     print "\x1B[31;1m#{num}\x1B[37;m".rjust(15)
   when num == today && num < 10 
     print "\x1B[31;1m#{num}\x1B[37;m".rjust(16)
   when num == today
     print "\x1B[31;1m#{num}\x1B[37;m".rjust(15)
-  when (youbi)%7 == 0 && num < 10
+  when (day_of_week)%7 == 0 && num < 10
     print "\x1B[36;1m#{num}\x1B[37;m".rjust(15)
-  when (youbi)%7 == 0 
+  when (day_of_week)%7 == 0 
     print "\x1B[36;1m#{num}\x1B[37;m"
   else 
     print " " + "\x1B[36;1m#{num}\x1B[37;m".rjust(15)
   end
-  if ((youbi+=1)%7 == 0)
+  if ((day_of_week+=1)%7 == 0)
     puts
   end
 end
