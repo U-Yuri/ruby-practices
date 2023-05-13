f1c0dc49d0cc4707eb79048663b6b2f5b31b7c72
yuriririn <80314978+yuriririn@users.noreply.github.com>
2023-05-13 18:17:44 +0900

    日付のスペース含めたレイアウト、今日の日付の色、カレンダーの色設定

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -7,7 +7,7 @@ opt = OptionParser.new
 opt.on('-y VAL') {|v| year = v.to_i }
 opt.on('-m VAL') {|v| month = v.to_i }
 opt.parse!(ARGV)
-puts "\x1B[36;1m#{month}\x1B[37;m月\x1B[36;1m#{year}\x1B[37;m".rjust(38)
+puts "     \x1B[36;1m#{month}\x1B[37;m月\x1B[36;1m#{year}\x1B[37;m"
 puts "日 月 火 水 木 金 土"
 end_of_date = Date.new(year, month, -1).day
 start_of_date = Date.new(year, month, 1).wday
@@ -18,12 +18,19 @@ end
 
 day_of_week = start_of_date
 today = day.day
+
 (1..end_of_date).each do |day|
+  day_colomn = "#{day}".rjust(2)
+  case 
+  when day == today
+    print "\x1B[31;1m" + day_colomn + "\x1B[37;m" + " "
+  else 
+    print "\x1B[36;1m" + day_colomn + "\x1B[37;m" + " "
+  end
+  
   case
   when day_of_week%7 == 6
-    puts day
-  else
-    print day
+    print "\n"
   end
   day_of_week = day_of_week + 1
  end
