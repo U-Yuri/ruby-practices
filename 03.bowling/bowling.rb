52ca6b6529f9d1e027df966dc90d592224202eee
yuriririn <80314978+yuriririn@users.noreply.github.com>
2023-05-19 16:44:05 +0900

    ストライクの１０ポイントに、時に次のスコア（array）の１投目と２投目を足した数を足すプログラムを追加

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -4,28 +4,31 @@ score = ARGV[0]
 scores = score.split(',')
 shots = []
 scores.each do |s|
-  if s == 'X' # strike
+  if s == 'X'
     shots << 10
     shots << 0
   else
     shots << s.to_i
   end
 end
-p shots
 
 frames = []
 shots.each_slice(2) do |s|
   frames << s
 end
+p frames
 
 point = 0
+index = 0
 frames.each do |frame|
+  next_array = frames[index + 1]
   if frame[0] == 10 # strike
-    point += 30
+    point += frame[0] + next_array[0] + next_array[1]
   elsif frame.sum == 10 # spare
     point += frame[0] + 10
   else
     point += frame.sum
   end
+  index += 1
 end
 puts point
