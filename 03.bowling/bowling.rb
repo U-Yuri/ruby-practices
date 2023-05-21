44e3fad844a391e592902c49e78d9b359ec3d08b
yuriririn <80314978+yuriririn@users.noreply.github.com>
2023-05-22 01:20:52 +0900

    コメントアウトしていたデバッガーの削除

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,5 +1,4 @@
 # ボウリングスコアプログラム
-require 'debug'
 
 score = ARGV[0]
 scores = score.split(',')
@@ -21,20 +20,18 @@ p frames
 
 point = 0
 index = 0
-# debugger
 frames.each do |frame|
-  # debugger
   next_array = frames[index + 1]
   
   case
   when frame[0] == 10 && index < 9
-    # debugger
       point += frame[0] + next_array[0] + next_array[1]
   when frame.sum == 10 && index < 9
       point += frame.sum + next_array[0] if index < 9
   else 
     point += frame.sum
   end
+
   print "index=#{index} frame=#{frame} point=#{point}\n"
   index += 1
 end
