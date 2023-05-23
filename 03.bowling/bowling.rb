277ae1896cb8e1ded97303df06b816689b195dd3
yuriririn <80314978+yuriririn@users.noreply.github.com>
2023-05-23 16:10:14 +0900

    ストライクのスコア計算方法が間違えていたので修正

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -2,6 +2,8 @@
 
 # ボウリングスコアプログラム
 
+require 'debug'
+
 score = ARGV[0]
 scores = score.split(',')
 shots = []
@@ -23,14 +25,18 @@ p frames
 point = 0
 index = 0
 frames.each do |frame|
+  # debugger
   next_array = frames[index + 1]
-
+  next_array_2 = frames[index + 2]
+  point += frame.sum
   if frame[0] == 10 && index < 9
-    point += frame[0] + next_array[0] + next_array[1]
+    if next_array[0] == 10 
+      point += next_array[0] + next_array_2[0]
+    else
+      point += next_array[0] + next_array[1]
+    end
   elsif frame.sum == 10 && index < 9
-    point += frame.sum + next_array[0]
-  else
-    point += frame.sum
+    point += next_array[0]
   end
 
   print "index=#{index} frame=#{frame} point=#{point}\n"
