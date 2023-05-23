fb0bbdb498a509b17e45135e89735225f0449d8c
yuriririn <80314978+yuriririn@users.noreply.github.com>
2023-05-23 16:13:45 +0900

    変数名のリファクタリング

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -23,17 +23,17 @@ p frames
 point = 0
 index = 0
 frames.each do |frame|
-  next_array = frames[index + 1]
+  next_array_1 = frames[index + 1]
   next_array_2 = frames[index + 2]
   point += frame.sum
   if frame[0] == 10 && index < 9
-    if next_array[0] == 10 
-      point += next_array[0] + next_array_2[0]
+    if next_array_1[0] == 10 
+      point += next_array_1[0] + next_array_2[0]
     else
-      point += next_array[0] + next_array[1]
+      point += next_array_1[0] + next_array_1[1]
     end
   elsif frame.sum == 10 && index < 9
-    point += next_array[0]
+    point += next_array_1[0]
   end
 
   print "index=#{index} frame=#{frame} point=#{point}\n"
