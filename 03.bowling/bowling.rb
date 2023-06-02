e960953b6d7138c362882bb2e83befd07caa3d33
yuriririn <80314978+yuriririn@users.noreply.github.com>
2023-06-02 14:04:43 +0900

    10本倒した時の数字がマジックナンバーになっていたので、変数に書き直した。

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -24,15 +24,16 @@ frames.each_with_index do |frame, number_of_times|
     next_array = frames[number_of_times + 1]
     next_next_array = frames[number_of_times + 2]
     point += frame.sum
-    if frame[0] == 10 && number_of_times < 9
+    all_pin = 10
+    if frame[0] == all_pin && number_of_times < 9
       point += next_array[0]
-      point += if next_array[0] == 10
+      point += if next_array[0] == all_pin
         next_next_array[0]
       else
         next_array[1]
       end
 
-    elsif frame.sum == 10 && number_of_times < 9
+    elsif frame.sum == all_pin && number_of_times < 9
       point += next_array[0]
     end
 end
