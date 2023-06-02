d17c30aeb1e731c65c950a1801478efe6869c9d9
yuriririn <80314978+yuriririn@users.noreply.github.com>
2023-06-02 13:53:18 +0900

    frames.eachの文をframes.each_with_index文に変更してindexをインクリメントする処理をなくした

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -20,23 +20,21 @@ shots.each_slice(2) do |s|
 end
 
 point = 0
-index = 0
-frames.each do |frame|
-  next_array = frames[index + 1]
-  next_next_array = frames[index + 2]
-  point += frame.sum
-  if frame[0] == 10 && index < 9
-    point += next_array[0]
-    point += if next_array[0] == 10
-               next_next_array[0]
-             else
-               next_array[1]
-             end
+frames.each_with_index do |frame, number_of_times|
+    next_array = frames[number_of_times + 1]
+    next_next_array = frames[number_of_times + 2]
+    point += frame.sum
+    if frame[0] == 10 && number_of_times < 9
+      point += next_array[0]
+      point += if next_array[0] == 10
+        next_next_array[0]
+      else
+        next_array[1]
+      end
 
-  elsif frame.sum == 10 && index < 9
-    point += next_array[0]
-  end
-
-  index += 1
+    elsif frame.sum == 10 && number_of_times < 9
+      point += next_array[0]
+    end
 end
+
 puts point
