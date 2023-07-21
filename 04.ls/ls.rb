224fce03b1226d81864a97ee3ab2fbc52f383a73
yuriririn <80314978+yuriririn@users.noreply.github.com>
2023-07-21 14:29:09 +0900

    make_lineに-rの時の挙動追加、オプション混在時の条件追加

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -22,8 +22,8 @@ def make_cell(rows, cols, files_ordered)
   end
 end
 
-def block_sum(files)
-  block_num = files.sum do |file|
+def block_sum(files_ordered)
+  block_num = files_ordered.sum do |file|
     File.stat(file.to_s).blocks
   end
   puts "total #{block_num}"
@@ -72,9 +72,9 @@ def other_print(file_path, file)
   print file
 end
 
-def make_line(files)
-  block_sum(files)
-  files.each do |file|
+def make_line(files, files_ordered)
+  block_sum(files_ordered)
+  files_ordered.each do |file|
     if File.ftype(file.to_s) == 'directory'
       print 'd'
     else
@@ -99,7 +99,9 @@ row_num = (files.size / COL_NUM.to_f).ceil
 cols = (0..COL_NUM - 1)
 rows = (0..row_num - 1)
 
-if option[:l]
+if option[:l] && [:a]
+  make_line(files, files_ordered)
+elsif option[:l]
   make_line(files)
 else
   make_cell(rows, cols, files_ordered)
