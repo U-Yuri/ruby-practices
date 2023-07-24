f6cb9b65d7e53c1fe1cc17528edf198a0446a15d
yuriririn <80314978+yuriririn@users.noreply.github.com>
2023-07-24 16:50:44 +0900

    とりあえず簡単に各オプション作成

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -1,16 +1,46 @@
-#ベース
 require 'optparse'
 
 opt = OptionParser.new
 option = {}
-opt.on('-l') { |a| option[:l] = l }
-opt.on('-w') { |r| option[:w] = w }
-opt.on('-c') { |l| option[:c] = c }
+opt.on('-l') { |l| option[:l] = l }
+opt.on('-w') { |w| option[:w] = w }
+opt.on('-c') { |c| option[:c] = c }
 opt.parse(ARGV)
 
-file = File.read("#{ARGV[0]}")
+if option[:l] || option[:w] || option[:c]
+  option_file = File.read("#{ARGV[1]}")
+else
+  file = File.read("#{ARGV[0]}")
+end
 
-print file.lines.count
-print file.split.size
-print file.size
-print "#{ARGV[0]}\n"
+def normal(file)
+  print file.lines.count.to_s.rjust(7)
+  print file.split.size.to_s.rjust(7)
+  print file.size.to_s.rjust(7)
+  print " #{ARGV[0]}\n"
+end
+
+def l(option_file)
+  print option_file.lines.count.to_s.rjust(7)
+  print " #{ARGV[1]}\n"
+end
+
+def w(option_file)
+  print option_file.split.size.to_s.rjust(7)
+  print " #{ARGV[1]}\n"
+end
+
+def c(option_file) 
+  print option_file.size.to_s.rjust(7)
+  print " #{ARGV[1]}\n"
+end
+
+if option[:l]
+  l(option_file)
+elsif option[:w]
+  w(option_file)
+elsif option[:c]
+  c(option_file)
+else
+  normal(file)
+end
