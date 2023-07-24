c5d2da539b386d05e4f385067f36fc7eb594d151
yuriririn <80314978+yuriririn@users.noreply.github.com>
2023-07-24 17:14:34 +0900

    ファイル取得の記述変更

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -5,42 +5,42 @@ option = {}
 opt.on('-l') { |l| option[:l] = l }
 opt.on('-w') { |w| option[:w] = w }
 opt.on('-c') { |c| option[:c] = c }
-opt.parse(ARGV)
-
-if option[:l] || option[:w] || option[:c]
-  option_file = File.read("#{ARGV[1]}")
-else
-  file = File.read("#{ARGV[0]}")
-end
+argv = opt.parse(ARGV)
 
 def normal(file)
   print file.lines.count.to_s.rjust(7)
   print file.split.size.to_s.rjust(7)
   print file.size.to_s.rjust(7)
-  print " #{ARGV[0]}\n"
 end
 
 def l(option_file)
   print option_file.lines.count.to_s.rjust(7)
-  print " #{ARGV[1]}\n"
 end
 
 def w(option_file)
   print option_file.split.size.to_s.rjust(7)
-  print " #{ARGV[1]}\n"
 end
 
 def c(option_file) 
   print option_file.size.to_s.rjust(7)
-  print " #{ARGV[1]}\n"
+end
+
+if option[:l] || option[:w] || option[:c]
+  option_file = File.read("#{argv[0]}")
+else
+  file = File.read("#{argv[0]}")
 end
 
 if option[:l]
   l(option_file)
+  print " #{argv[0]}\n"
 elsif option[:w]
   w(option_file)
+  print " #{argv[0]}\n"
 elsif option[:c]
   c(option_file)
+  print " #{argv[0]}\n"
 else
   normal(file)
+  print " #{argv[0]}\n"
 end
