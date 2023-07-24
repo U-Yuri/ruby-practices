3af5c7a0aec02387e6368f0ba866b35bdbf7369d
yuriririn <80314978+yuriririn@users.noreply.github.com>
2023-07-24 11:01:03 +0900

    オプションの設定のみ記載

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -1,6 +1,13 @@
 #ベース
 require 'optparse'
 
+opt = OptionParser.new
+option = {}
+opt.on('-l') { |a| option[:l] = l }
+opt.on('-w') { |r| option[:w] = w }
+opt.on('-c') { |l| option[:c] = c }
+opt.parse(ARGV)
+
 file = File.read("#{ARGV[0]}")
 
 print file.lines.count
