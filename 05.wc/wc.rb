3aa5b96bf94e62d8923ac807d3b6abf0ec517ad8
yuriririn <80314978+yuriririn@users.noreply.github.com>
2023-07-25 17:01:10 +0900

    パイプ含めたコード完成

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -1,5 +1,7 @@
 require 'optparse'
+require 'debug'
 
+# debugger
 opt = OptionParser.new
 option = {}
 opt.on('-l') { |l| option[:l] = l }
@@ -7,10 +9,23 @@ opt.on('-w') { |w| option[:w] = w }
 opt.on('-c') { |c| option[:c] = c }
 argv = opt.parse(ARGV)
 
-file = File.read("#{argv[0]}")
-option_l = file.lines.count.to_s.rjust(7)
-option_w = file.split.size.to_s.rjust(7)
-option_c = file.size.to_s.rjust(7)
+if argv[0] == nil
+  file = []
+  while true
+    file_name = $stdin.gets
+    if file_name == nil
+      break
+    end
+    file.push("#{file_name}")
+  end
+  p file = file.join
+else
+  file = File.read("#{argv[0]}")
+end
+
+option_l = file.lines.count.to_s.rjust(8)
+option_w = file.split.size.to_s.rjust(8)
+option_c = file.size.to_s.rjust(8)
 
 print option_l if option[:l]
 print option_w if option[:w]
