187614e20d00f9ddc6cc7603d1c1161a052c7494
yuriririn <80314978+yuriririn@users.noreply.github.com>
2023-08-01 15:34:30 +0900

    複数のファイル指定に対応

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -18,17 +18,10 @@ if argv[0].nil?
     file.push(file_name.to_s)
   end
   file = file.join
-else
-  file = File.read(argv[0].to_s)
-end
 
-option_l = file.lines.count.to_s.rjust(8)
-option_w = file.split.size.to_s.rjust(8)
-option_c = file.size.to_s.rjust(8)
-
-print option_l if option[:l]
-print option_w if option[:w]
-print option_c if option[:c]
+  option_l = file.lines.count.to_s.rjust(8)
+  option_w = file.split.size.to_s.rjust(8)
+  option_c = file.size.to_s.rjust(8)
 
 if option == {}
   print option_l
@@ -37,3 +30,55 @@ if option == {}
 end
 
 print " #{argv[0]}\n"
+
+else
+  option_l_total_num = 0
+  option_w_total_num = 0
+  option_c_total_num = 0
+
+  argv.each do |file_name|
+    file = File.read(file_name)
+    option_l = file.lines.count.to_s.rjust(8)
+    option_w = file.split.size.to_s.rjust(8)
+    option_c = file.size.to_s.rjust(8)
+
+    option_l_total_num += file.lines.count
+    option_w_total_num += file.split.size
+    option_c_total_num += file.size
+
+    print option_l if option[:l]
+    print option_w if option[:w]
+    print option_c if option[:c]
+  
+    if option == {}
+      print option_l
+      print option_w
+      print option_c
+    end
+  
+    print " #{argv[0]}\n"
+  end
+
+  if option[:l]
+    print option_l_total_num.to_s.rjust(8)
+    print " total\n"
+  end
+
+  if option[:w]
+    print option_w_total_num.to_s.rjust(8)
+    print " total\n"
+  end
+
+  if option[:c]
+    print option_c_total_num.to_s.rjust(8)
+    print " total\n"
+  end
+
+  if argv.size >= 2 && option == {}
+    print option_l_total_num.to_s.rjust(8)
+    print option_w_total_num.to_s.rjust(8)
+    print option_c_total_num.to_s.rjust(8)
+    print " total\n"
+  end
+
+end
