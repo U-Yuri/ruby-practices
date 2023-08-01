29a2e2b35f51dd775eacdee87e230112701ed6f3
yuriririn <80314978+yuriririn@users.noreply.github.com>
2023-08-01 16:11:36 +0900

    よく出てくるコードをメソッドに入れた

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -9,16 +9,7 @@ opt.on('-w') { |w| option[:w] = w }
 opt.on('-c') { |c| option[:c] = c }
 argv = opt.parse(ARGV)
 
-if argv[0].nil?
-  file = []
-  loop do
-    file_name = $stdin.gets
-    break if file_name.nil?
-
-    file.push(file_name.to_s)
-  end
-  file = file.join
-
+def option_or_no_option(file, option, argv)
   option_l = file.lines.count.to_s.rjust(8)
   option_w = file.split.size.to_s.rjust(8)
   option_c = file.size.to_s.rjust(8)
@@ -35,6 +26,20 @@ if argv[0].nil?
 
   print " #{argv[0]}\n"
 
+end
+
+if argv[0].nil?
+  file = []
+  loop do
+    file_name = $stdin.gets
+    break if file_name.nil?
+
+    file.push(file_name.to_s)
+  end
+  file = file.join
+
+  option_or_no_option(file, option, argv)
+
 else
   option_l_total_num = 0
   option_w_total_num = 0
@@ -42,25 +47,13 @@ else
 
   argv.each do |file_name|
     file = File.read(file_name)
-    option_l = file.lines.count.to_s.rjust(8)
-    option_w = file.split.size.to_s.rjust(8)
-    option_c = file.size.to_s.rjust(8)
 
+    option_or_no_option(file, option, argv)
+    
     option_l_total_num += file.lines.count
     option_w_total_num += file.split.size
     option_c_total_num += file.size
 
-    print option_l if option[:l]
-    print option_w if option[:w]
-    print option_c if option[:c]
-
-    if option == {}
-      print option_l
-      print option_w
-      print option_c
-    end
-
-    print " #{argv[0]}\n"
   end
 
   if option[:l]
