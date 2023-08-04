9dca988ab1224e53bf0e2418c670ce9729bbdf22
yuriririn <80314978+yuriririn@users.noreply.github.com>
2023-08-04 14:51:30 +0900

    putsに変更した際\nを削除し忘れていたので削除、変数名を「files」へ変更

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -9,10 +9,10 @@ opt.on('-w') { |w| option[:w] = w }
 opt.on('-c') { |c| option[:c] = c }
 argv = opt.parse(ARGV)
 
-def option_or_no_option(file, option, argv)
-  option_l = file.lines.count.to_s.rjust(8)
-  option_w = file.split.size.to_s.rjust(8)
-  option_c = file.size.to_s.rjust(8)
+def option_or_no_option(files, option, argv)
+  option_l = files.lines.count.to_s.rjust(8)
+  option_w = files.split.size.to_s.rjust(8)
+  option_c = files.size.to_s.rjust(8)
 
   print option_l if option[:l]
   print option_w if option[:w]
@@ -24,20 +24,20 @@ def option_or_no_option(file, option, argv)
     print option_c
   end
 
-  puts " #{argv[0]}\n"
+  puts " #{argv[0]}"
 end
 
 if argv[0].nil?
-  file = []
+  files = []
   loop do
     file_name = $stdin.gets
     break if file_name.nil?
 
-    file.push(file_name.to_s)
+    files.push(file_name.to_s)
   end
-  file = file.join
+  files = files.join
 
-  option_or_no_option(file, option, argv)
+  option_or_no_option(files, option, argv)
 
 else
   option_l_total_num = 0
@@ -45,13 +45,13 @@ else
   option_c_total_num = 0
 
   argv.each do |file_name|
-    file = File.read(file_name)
+    files = File.read(file_name)
 
-    option_or_no_option(file, option, argv)
+    option_or_no_option(files, option, argv)
 
-    option_l_total_num += file.lines.count
-    option_w_total_num += file.split.size
-    option_c_total_num += file.size
+    option_l_total_num += files.lines.count
+    option_w_total_num += files.split.size
+    option_c_total_num += files.size
   end
 
   print option_l_total_num.to_s.rjust(8) if option[:l]
@@ -63,5 +63,5 @@ else
     print option_w_total_num.to_s.rjust(8)
     print option_c_total_num.to_s.rjust(8)
   end
-  puts " total\n"
+  puts " total"
 end
