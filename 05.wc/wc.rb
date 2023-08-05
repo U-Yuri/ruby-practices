e049db44d42b33cadc69fe465e537617abdfedbc
yuriririn <80314978+yuriririn@users.noreply.github.com>
2023-08-05 13:53:18 +0900

    一度メソッドを解除

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -13,7 +13,12 @@ def main
 
   if argv[0].nil?
     lines = $stdin.read
-    option_or_no_option(lines, option, argv)
+    option_l = lines.lines.count
+    option_w = lines.split.size
+    option_c = lines.size
+
+    print_count(option_l, option_w, option_c, option)
+    puts " #{argv[0]}"
 
   else
     option_l_total_num = 0
@@ -23,7 +28,12 @@ def main
     argv.each do |file_name|
       file = File.read(file_name)
 
-      option_or_no_option(file, option, argv)
+      option_l = file.lines.count
+      option_w = file.split.size
+      option_c = file.size
+
+      print_count(option_l, option_w, option_c, option)
+      puts " #{argv[0]}"
 
       option_l_total_num += file.lines.count
       option_w_total_num += file.split.size
@@ -33,15 +43,6 @@ def main
     print_count(option_l_total_num, option_w_total_num, option_c_total_num, option)
     puts " total"
   end
-end
-
-def option_or_no_option(files, option, argv)
-  option_l = files.lines.count
-  option_w = files.split.size
-  option_c = files.size
-
-  print_count(option_l, option_w, option_c, option)
-  puts " #{argv[0]}"
 end
 
 def print_count(option_l, option_w, option_c, option)
