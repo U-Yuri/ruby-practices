5800842caf1955cea76d3629b733b219d57cbc5a
yuriririn <80314978+yuriririn@users.noreply.github.com>
2023-08-05 13:40:46 +0900

    printに関する似たようなコードをリファクタリング

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -29,10 +29,8 @@ def main
       option_w_total_num += file.split.size
       option_c_total_num += file.size
     end
-
-    print option_l_total_num.to_s.rjust(8) if option[:l] || option == {}
-    print option_w_total_num.to_s.rjust(8) if option[:w] || option == {}
-    print option_c_total_num.to_s.rjust(8) if option[:c] || option == {}
+    
+    print_count(option_l_total_num, option_w_total_num, option_c_total_num, option)
     puts " total"
   end
 end
@@ -42,10 +40,14 @@ def option_or_no_option(files, option, argv)
   option_w = files.split.size
   option_c = files.size
 
+  print_count(option_l, option_w, option_c, option)
+  puts " #{argv[0]}"
+end
+
+def print_count(option_l, option_w, option_c, option)
   print option_l.to_s.rjust(8) if option[:l] || option == {}
   print option_w.to_s.rjust(8) if option[:w] || option == {}
   print option_c.to_s.rjust(8) if option[:c] || option == {}
-  puts " #{argv[0]}"
 end
 
 main
