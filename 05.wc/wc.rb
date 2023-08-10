3f4570bbb29dcc2f9630bb81197cc770c9fde89d
Yuri <80314978+U-Yuri@users.noreply.github.com>
2023-08-10 16:00:01 +0900

    empty?使用、パイプ時にバグあり修正、mainが長すぎるので条件をメソッドに定義。

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -3,44 +3,26 @@
 require 'optparse'
 
 def main
-  _opt, option, argv = set_option
+  option, argv = set_option
 
   if argv[0].nil?
-    deta = $stdin.read
-    option_l, option_w, option_c, show_count = a(deta, argv, option)
-
+    count_and_print_stdin(argv, option)
   else
-    option_l_total_num = 0
-    option_w_total_num = 0
-    option_c_total_num = 0
-
-    argv.each do |file_name|
-      deta = File.read(file_name)
-      option_l, option_w, option_c, show_count = a(deta, file_name, option)
-
-      option_l_total_num += option_l
-      option_w_total_num += option_w
-      option_c_total_num += option_c
-    end
-
-    print_count(option_l_total_num, option_w_total_num, option_c_total_num, option)
-    puts ' total'
+    count_and_print_files(argv, option)
   end
 end
 
 def print_count(option_l, option_w, option_c, option)
-  print option_l.to_s.rjust(8) if option[:l] || option == {}
-  print option_w.to_s.rjust(8) if option[:w] || option == {}
-  print option_c.to_s.rjust(8) if option[:c] || option == {}
+  print option_l.to_s.rjust(8) if option[:l] || option.empty?
+  print option_w.to_s.rjust(8) if option[:w] || option.empty?
+  print option_c.to_s.rjust(8) if option[:c] || option.empty?
 end
 
 def a(deta, file_name, option)
   option_l = deta.lines.count
   option_w = deta.split.size
   option_c = deta.size
-  show_count = print_count(option_l, option_w, option_c, option)
-  puts " #{file_name}"
-  [option_l, option_w, option_c, show_count]
+  [option_l, option_w, option_c]
 end
 
 def set_option
@@ -50,7 +32,33 @@ def set_option
   opt.on('-w') { |w| option[:w] = w }
   opt.on('-c') { |c| option[:c] = c }
   argv = opt.parse(ARGV)
-  [opt, option, argv]
+  [option, argv]
+end
+
+def count_and_print_files(argv, option)
+  option_l_total_num = 0
+  option_w_total_num = 0
+  option_c_total_num = 0
+
+  argv.each do |file_name|
+    deta = File.read(file_name)
+    option_l, option_w, option_c= a(deta, file_name, option)
+    print_count(option_l, option_w, option_c, option)
+    puts " #{file_name}"
+    option_l_total_num += option_l
+    option_w_total_num += option_w
+    option_c_total_num += option_c
+  end
+
+  print_count(option_l_total_num, option_w_total_num, option_c_total_num, option)
+  puts ' total'
+end
+
+def count_and_print_stdin(argv, option)
+  deta = $stdin.read
+    option_l, option_w, option_c = a(deta, argv, option)
+    print_count(option_l, option_w, option_c, option)
+  puts
 end
 
 main
